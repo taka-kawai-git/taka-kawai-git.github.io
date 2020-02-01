cbb00038712eb9ce0cb689e4f821e37a33ffdc3a
taka-kawai-git <taka.kawai.g@gmail.com>
2020-02-01 10:20:34 +0000

    add Menu.vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="menu" class="container">
+    <ul class="collection border-0">
+        <li class="collection-item p-x-0 bg-theme"><button v-on:click="logout" class="btn black rounded-5">Logout</button></li>
+    </ul>
+</div>
+</template>
+
+<script>
+import firebase from 'firebase';
+export default {
+    name : 'menu',
+    data() {
+        return {
+        }
+    },
+    methods: {
+        logout: function() {
+            firebase.auth().signOut().then(() => {
+                this.$router.go({path: this.$router.path});
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+    @import "../assets/common.css";
+</style>
