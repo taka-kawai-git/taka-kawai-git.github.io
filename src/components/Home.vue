8b14b0fb6661ebf01f9e737c63cbff653f5c6b04
taka-kawai-git <taka.kawai.g@gmail.com>
2020-02-27 11:48:59 +0000

    add popular contents

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,20 @@
 <template>
 <div id="home" class="container-100">
+
+    <!-------- Tabs -------->
+
     <ul id="tabs-swipe-ul" class="tabs bg-none">
         <li class="tab col s3"><a class="active" href="#tab-swipe-1">最新</a></li>
         <li class="tab col s3"><a href="#tab-swipe-2">話題</a></li>
+        <li class="tab col s3"><a href="#tab-swipe-3">投票</a></li>
     </ul>
+
+    <!-------- Latest -------->
+
     <div id="tab-swipe-1" class="col s12">
         <ul class="collection border-0 b-color-theme fs-1-1 m-y-0">
-            <li v-for="thread in threads" v-bind:key="thread.id"
-            class="collection-item p-x-0 bg-none b-color-theme">
+            <li v-for="thread in threads_latest" v-bind:key="thread.id"
+            class="collection-item p-x-0 bg-none b-color-theme l-h-2-5">
                 <router-link class="black-text"
                 v-bind:to="{ name:'view-thread', params: { thread_id: thread.thread_id } }">
                 <div class="container-sub">{{ thread.title }}</div>
@@ -15,7 +22,28 @@
             </li>
         </ul>
     </div>
-    <div id="tab-swipe-2" class="col s12">Test 2</div>
+
+    <!-------- Popular -------->
+
+    <div id="tab-swipe-2" class="col s12">
+        <ul class="collection border-0 b-color-theme fs-1-1 m-y-0">
+            <li v-for="thread in threads_popular" v-bind:key="thread.id"
+            class="collection-item p-x-0 bg-none b-color-theme l-h-2-5">
+                <router-link class="black-text"
+                v-bind:to="{ name:'view-thread', params: { thread_id: thread.thread_id } }">
+                <div class="container-sub">{{ thread.title }}</div>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+
+    <!-------- Popular -------->
+
+    <div id="tab-swipe-3" class="col s12">
+    </div>
+
+    <!-------- Add-Comment button -------->
+
     <div class="fixed-action-btn">
         <router-link to="/new" class="btn-floating btn-large bg-theme-grad z-depth-1">
             <i class="fa fa-plus"></i>
@@ -33,7 +61,8 @@ export default {
     name : 'home',
     data() {
         return {
-            threads: []
+            threads_latest: [],
+            threads_popular: []
         }
     },
     mounted() {
@@ -50,9 +79,13 @@ export default {
                 const data = {
                     'id' : doc.id,
                     'title' : doc.data().title,
-                    'thread_id' : doc.data().thread_id
+                    'thread_id' : doc.data().thread_id,
+                    'num_comments': doc.data().comments.length
                 }
-                this.threads.push(data)
+                this.threads_latest.push(data);
+                this.threads_popular.push(data);
+                this.threads_popular.sort(
+                    (a, b) => b.num_comments - a.num_comments);
             })
         })
     }
