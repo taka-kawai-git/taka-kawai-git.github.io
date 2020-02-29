23c67115e8ef38c7cc1f9f1df6b5492b4cb3e0e9
taka-kawai-git <taka.kawai.g@gmail.com>
2020-02-29 16:02:50 +0000

    Change title to company

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  VUE_APP_SERVICE_NAME: 'Company',
   VUE_APP_NUM_SHARD: 10,
   VUE_APP_MAX_NUM_COMMENTS: 1000
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
+  VUE_APP_SERVICE_NAME: 'Company',
   VUE_APP_NUM_SHARD: 10,
   VUE_APP_MAX_NUM_COMMENTS: 1000
 }
