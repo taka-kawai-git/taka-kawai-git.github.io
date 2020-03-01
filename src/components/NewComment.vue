bf541e438f0faa0d61f335880432b79496ecd474
taka-kawai-git <taka.kawai.g@gmail.com>
2020-03-01 08:54:59 +0000

    update button design

--- src/components/NewComment.vue
+++ src/components/NewComment.vue
@@ -1,10 +1,11 @@
 <template>
-<div id="new-comment" class="container m-t-1">
-    <div class="row">
+<div id="new-comment" class="container">
+    <div class="row m-t-1">
         <form @submit.prevent="saveComment" class="col s12 p-x-0 fs-1-2">
             <textarea class="grey lighten-3 border-0 rounded-10 h-10 p-1"
             placeholder="コメント" v-model="comment" required></textarea>
-            <button type="submit" class="btn h-3 rounded-10 w-50 m-y-1 bg-theme-grad right">コメントする</button>
+            <button type="submit" class="btn waves-effect waves-light h-3 rounded-10 w-50 m-y-1
+            bg-theme z-depth-0 right">コメントする</button>
             <router-link to="/" class="m-y-1 left fs-1">キャンセル</router-link>
         </form>
     </div>
--- src/components/NewThread.vue
+++ src/components/NewThread.vue
@@ -1,13 +1,14 @@
 <template>
-<div id="new-thread" class="container m-t-1">
-    <div class="row">
+<div id="new-thread" class="container">
+    <div class="row m-t-1">
         <form @submit.prevent="saveThread" class="col s12 p-x-0 fs-1-2">
             <input type="text" class="grey lighten-3 border-0 rounded-10"
             placeholder="スレッドタイトル" v-model="title" required>
             <textarea class="grey lighten-3 border-0 rounded-10 h-10 p-1"
             placeholder="最初のコメント" v-model="comment" required></textarea>
-            <button type="submit" class="btn h-3 rounded-10 w-50 m-y-1 bg-theme-grad right fs-1">作成する</button>
-            <router-link to="/" class="m-y-1 left">キャンセル</router-link>
+            <button type="submit" class="btn waves-effect waves-light h-3 rounded-10 w-50 m-y-1
+            bg-theme z-depth-0 right fs-1">作成する</button>
+            <router-link to="/" class="m-y-1 left fs-1">キャンセル</router-link>
         </form>
     </div>
 </div>
