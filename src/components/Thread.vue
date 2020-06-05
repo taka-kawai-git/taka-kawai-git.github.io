621c1d7db1ee659b5cdd628a756073675308731c
taka-kawai-git <taka.kawai.g@gmail.com>
2020-06-05 06:00:45 +0000

    change icon to user icon

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -12,10 +12,10 @@
                         <td class="p-0 w-3-5 v-align-t p-r-1 center">
                             <div class="">
                                 <a href="#new-comment" v-on:click="addReferenceToTextarea(index)" class="modal-trigger">
-                                    <div class="fs-1 icon v-middle fw-b blue lighten-5 rounded-30">
+                                    <div class="fs-1 t-cell v-middle fw-b white rounded-30">
                                     <!--v-bind:class="colorObg(comment.user_id)">-->
-                                        {{ comment.user_id.slice(0, 1).toLowerCase() }}</div>
-                                        <!--<img class="" src="../assets/image/user.svg"/></div>-->
+                                        <!--{{ comment.user_id.slice(0, 1).toLowerCase() }}</div>-->
+                                        <img class="icon" src="../assets/image/user.svg"/></div>
                                 </a>
                             </div>
                         </td>
@@ -60,10 +60,10 @@
                         <td class="p-0 w-3-5 center v-align-t p-r-1">
                             <div class="">
                                 <a href="#new-comment" v-on:click="addReferenceToTextarea(index + checkedAt)" class="modal-trigger">
-                                    <div class="fs-1 icon v-middle fw-b blue lighten-5 rounded-30">
+                                    <div class="fs-1 t-cell v-middle fw-b white rounded-30">
                                         <!--v-bind:class="colorObg(comment.user_id)">-->
-                                        {{ comment.user_id.slice(0, 1).toLowerCase() }}</div>
-                                        <!--<img class="" src="../assets/image/user.svg"/></div>-->
+                                        <!--{{ comment.user_id.slice(0, 1).toLowerCase() }}</div>-->
+                                        <img class="icon" src="../assets/image/user.svg"/></div>
                                 </a>
                             </div>
                         </td>
